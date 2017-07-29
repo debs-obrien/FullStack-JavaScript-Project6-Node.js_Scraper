f9f581e3c30b8f943cd99b52c4822517db7ffc80
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-29 16:58:33 +0200

    working version of scrape just need to add url

--- scraper.js
+++ scraper.js
@@ -21,14 +21,12 @@ if(!isDirSync('data')){
 }
 //scrapes info from website and prints to console
 const scrapeIt = require("scrape-it");
-
-tshirts = {
-
-}
+const mainURL = "http://www.shirts4mike.com/";
+let url;
 let urls = [];
 //this only gives me one url need to find a way to get all foreach/for in/for ??
     scrapeIt("http://www.shirts4mike.com/shirts.php", {
-        pageLinks: {
+        tshirtLinks: {
             listItem: ".products li",
             data: {
                 links: {
@@ -37,33 +35,32 @@ let urls = [];
                 }
             }
             }
-    }).then(pageurl => {
-        console.log(pageurl);
-        pageurl.pageLinks.forEach(function(element){
+    }).then(pageurls => {
+        //console.log(pageurl);
+        pageurls.tshirtLinks.forEach(function(element){
             urls.push(element.links);
-        })
+        });
+        for(url in urls){
+             url = mainURL + urls[url];
+
+            scrapeIt(url, {
+                title: "title",
+                price: ".wrapper .price",
+                image: {
+                    selector: ".shirt-picture img",
+                    attr: "src"
+                }
+            }).then(shirtDetails => {
+                console.log(shirtDetails);
+            });
+        }
+
         //urls.push(pageurl); // pushes this to array{ pages: 'shirt.php?id=101' }
-        console.log(urls);
-        return urls;
+        //console.log(urls);
+        //return urls;
     });
-//this should be dynamic
+/*//this should be dynamic
 urls = ["shirt.php?id=101",
     "shirt.php?id=102",
-    "shirt.php?id=103"];
+    "shirt.php?id=103"];*/
 
-//this is working great
-for(url in urls){
-    var url = 'http://www.shirts4mike.com/' + urls[url];
-
-scrapeIt(url, {
-    title: "title",
-    price: ".wrapper .price",
-    image: {
-        selector: ".shirt-picture img",
-        attr: "src"
-    }
-
-}).then(page => {
-    console.log(page);
-});
-}
