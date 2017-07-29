6364cf3759e48b16582199a95a99e52ba3a3cc86
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-29 20:45:41 +0200

    added error message for when site is down

--- scraper.js
+++ scraper.js
@@ -14,66 +14,78 @@ const fields = ['Title', 'Price', 'Image Url', 'Url', 'Time'];
 let date = new Date();
 let csvFileName = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
 
-//looks to see if there is a specific directoy and if there isnt it makes it
+//looks to see if there is a specific directory and if there isn't it makes it
 function isDirSync(aPath){
     try{
         return fs.statSync(aPath).isDirectory();
-    }catch (e){
-        if(e.code === 'ENOENT'){
+    }catch (error){
+        if(error.code === 'ENOENT'){
             return false;
         }else {
-            throw e;
+            throw error;
         }
     }
 }
 if(!isDirSync('data')){
     fs.mkdirSync('data');
 }
-//scrapes info from website and prints to console
-    scrapeIt(shirtsURL, {
-        tshirtLinks: {
-            listItem: ".products li",
-            data: {
-                links: {
-                    selector: "a",
-                    attr: "href"
+//scrapes info from website and prints to csv file
+function scrapeData(site){
+        scrapeIt(site, {
+            tshirtLinks: {
+                listItem: ".products li",
+                data: {
+                    links: {
+                        selector: "a",
+                        attr: "href"
+                    }
                 }
             }
-        }
-    }).then(pageurls => {
-        //console.log(pageurl);
-        pageurls.tshirtLinks.forEach(function(element){
-            urls.push(element.links);
-        });
-        for(url in urls){
-             url = mainURL + urls[url];
-            scrapeIt(url, {
-                title: "title",
-                price: ".wrapper .price",
-                image: {
-                    selector: ".shirt-picture img",
-                    attr: "src"
-                }
-            }).then(shirtDetails => {
-                var tshirts = [
-                    {
-                        "Title": shirtDetails.title,
-                        "Price": shirtDetails.price,
-                        "Image Url": mainURL + shirtDetails.image,
-                        "Url": url,
-                        "Time": date.getTime()
-                    }
-                ];
-
-                //console.log(tshirts)
-                var csv = json2csv({data: tshirts, fields:fields});
-                console.log(csv);
-                fs.writeFile('data/'+ csvFileName +'.csv', csv, function(error){
-                    if(error) throw error;
-                    //console.log('file saved');
-                })
-
+        }).then(pageurls => {
+            pageurls.tshirtLinks.forEach(function(element){
+                urls.push(element.links);
             });
-        }
-    });
+            for(url in urls){
+                 url = mainURL + urls[url];
+                scrapeIt(url, {
+                    title: "title",
+                    price: ".wrapper .price",
+                    image: {
+                        selector: ".shirt-picture img",
+                        attr: "src"
+                    }
+                }).then(shirtDetails => {
+                    var tshirts = [
+                        {
+                            "Title": shirtDetails.title,
+                            "Price": shirtDetails.price,
+                            "Image Url": mainURL + shirtDetails.image,
+                            "Url": url,
+                            "Time": date.getTime()
+                        }
+                    ];
+                    //console.log(tshirts)
+                    var csv = json2csv({data: tshirts, fields:fields});
+                    console.log(csv);
+                    fs.writeFile('data/'+ csvFileName +'.csv', csv, function(error){
+                        if(error) throw error;
+                    })
 
+                });
+            }
+        }).catch(function(){
+            let errorLog = 'scraper-error.log';
+            let errorMessage= `${date.toString()} There’s been a 404 error. Cannot connect to ${mainURL}.\n`;
+            console.log(errorMessage);
+            if(!errorLog){
+                fs.writeFile(errorLog, errorMessage);
+            }else{
+                fs.appendFile(errorLog, errorMessage);
+            }
+        });
+    }
+
+scrapeData(shirtsURL);
+
+//TODO
+//Edit your package.json file so that your program runs when the npm start command is run.
