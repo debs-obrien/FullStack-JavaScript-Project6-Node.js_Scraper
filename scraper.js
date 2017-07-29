7b6035a917f96aafd1411e457a3db305e250b565
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-29 20:18:28 +0200

    refactoring urls

--- scraper.js
+++ scraper.js
@@ -4,9 +4,14 @@
 //const scrapeIt = require("./scrape-it");
 const fs = require('fs');
 const json2csv = require('json2csv');
+const scrapeIt = require("scrape-it");
+
+const shirtsURL = "http://www.shirts4mike.com/shirts.php";
+const mainURL = "http://www.shirts4mike.com/";
+let url;
+let urls = [];
 const fields = ['Title', 'Price', 'Image Url', 'Url', 'Time'];
 let date = new Date();
-
 let csvFileName = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
 
 //looks to see if there is a specific directoy and if there isnt it makes it
@@ -25,11 +30,7 @@ if(!isDirSync('data')){
     fs.mkdirSync('data');
 }
 //scrapes info from website and prints to console
-const scrapeIt = require("scrape-it");
-const mainURL = "http://www.shirts4mike.com/";
-let url;
-let urls = [];
-    scrapeIt("http://www.shirts4mike.com/shirts.php", {
+    scrapeIt(shirtsURL, {
         tshirtLinks: {
             listItem: ".products li",
             data: {
@@ -38,7 +39,7 @@ let urls = [];
                     attr: "href"
                 }
             }
-            }
+        }
     }).then(pageurls => {
         //console.log(pageurl);
         pageurls.tshirtLinks.forEach(function(element){
@@ -59,7 +60,7 @@ let urls = [];
                         "Title": shirtDetails.title,
                         "Price": shirtDetails.price,
                         "Image Url": mainURL + shirtDetails.image,
-                        "Url": 'url needs to be fixed',
+                        "Url": url,
                         "Time": date.getTime()
                     }
                 ];
