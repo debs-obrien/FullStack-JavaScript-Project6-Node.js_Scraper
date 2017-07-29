b3857a6f3f3a96c131330f18c565e760cfb472cc
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-29 16:44:25 +0200

    scraping over list to get page links

--- scraper.js
+++ scraper.js
@@ -25,19 +25,45 @@ const scrapeIt = require("scrape-it");
 tshirts = {
 
 }
+let urls = [];
+//this only gives me one url need to find a way to get all foreach/for in/for ??
+    scrapeIt("http://www.shirts4mike.com/shirts.php", {
+        pageLinks: {
+            listItem: ".products li",
+            data: {
+                links: {
+                    selector: "a",
+                    attr: "href"
+                }
+            }
+            }
+    }).then(pageurl => {
+        console.log(pageurl);
+        pageurl.pageLinks.forEach(function(element){
+            urls.push(element.links);
+        })
+        //urls.push(pageurl); // pushes this to array{ pages: 'shirt.php?id=101' }
+        console.log(urls);
+        return urls;
+    });
+//this should be dynamic
+urls = ["shirt.php?id=101",
+    "shirt.php?id=102",
+    "shirt.php?id=103"];
 
-scrapeIt("http://www.shirts4mike.com/shirt.php", {
-    //title: "title",
-    //price: ".wrapper .price",
-         url: {
-            selector: ".products a",
-            attr: "href"
-        }
-    /*image: {
+//this is working great
+for(url in urls){
+    var url = 'http://www.shirts4mike.com/' + urls[url];
+
+scrapeIt(url, {
+    title: "title",
+    price: ".wrapper .price",
+    image: {
         selector: ".shirt-picture img",
         attr: "src"
-    }*/
+    }
 
 }).then(page => {
     console.log(page);
 });
+}
